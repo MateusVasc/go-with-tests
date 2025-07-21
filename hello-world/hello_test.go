64b88984a2619295cdd9de3446a17faa1c87beda
MateusVasc <mvaraujo2004@gmail.com>
2025-07-21 09:28:28 -0300

    feat: adds hello world unit test

---
+++ hello-world/hello_test.go
@@ -0,0 +1,12 @@
+package gowithtests
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	got := hello()
+	want := "Hello, World"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
